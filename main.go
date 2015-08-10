16cf3f76714002c16f86c65f32bbd9d8dfef200b
Zachary Huff <zach.huff.386@gmail.com>
2015-08-09 21:21:28 -0400

    Add version cmd

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codegangsta/cli"
 	"github.com/cortunl/cortunl/constants"
 	"os"
@@ -10,5 +11,16 @@ func main() {
 	app := cli.NewApp()
 	app.Name = constants.NameFormated
 	app.Version = constants.Version
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "version",
+			Usage: "Print app version",
+			Action: func(c *cli.Context) {
+				fmt.Printf("v%s\n", constants.Version)
+			},
+		},
+	}
+
 	app.Run(os.Args)
 }

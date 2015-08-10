3c45f90bdee719167f8eb36d9870b50bd51ee914
Zachary Huff <zach.huff.386@gmail.com>
2015-08-09 23:11:14 -0400

    Update main

--- main.go
+++ main.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"github.com/codegangsta/cli"
-	"github.com/cortunl/cortunl/constants"
-	"os"
+	"github.com/cortunl/cortunl/cmd"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = constants.NameFormated
-	app.Version = constants.Version
-
-	app.Commands = []cli.Command{
-		{
-			Name:  "version",
-			Usage: "Print app version",
-			Action: func(c *cli.Context) {
-				fmt.Printf("v%s\n", constants.Version)
-			},
-		},
+	_, err := cmd.Run()
+	if err != nil {
+		return
 	}
-
-	app.Run(os.Args)
 }

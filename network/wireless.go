f589705960899c9f57f98a92a0556b18beb7a681
Zachary Huff <zach.huff.386@gmail.com>
2015-09-06 02:13:14 -0400

    Remove secuirty type

--- network/wireless.go
+++ network/wireless.go
@@ -1,19 +1,13 @@
 package network
 
-const (
-	Wep  SecurityType = "wep"
-	Wpa  SecurityType = "wpa"
-	Wpa2 SecurityType = "wpa2"
-	None SecurityType = "none"
+import (
+	"github.com/cortunl/cortunl/security"
 )
-
-type SecurityType string
 
 type WirelessNetwork struct {
 	*Network
-	Ssid         string
-	Quality      int
-	Channel      int
-	Security     SecurityType
-	SecurityData interface{}
+	Ssid     string
+	Quality  int
+	Channel  int
+	Security security.Security
 }

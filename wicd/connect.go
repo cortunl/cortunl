07461f28058ac381138e63f9ccd480989b55c309
Zachary Huff <zach.huff.386@gmail.com>
2015-09-07 07:13:07 -0400

    Add connect wired

--- wicd/connect.go
+++ wicd/connect.go
@@ -5,10 +5,28 @@ import (
 	"github.com/pacur/pacur/utils"
 )
 
+func connectWired() (err error) {
+	conn, err := wired()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	_, err = conn.Call("ConnectWired")
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 func Connect(netIntf interface{}) (err error) {
 	switch net := netIntf.(type) {
 	case *network.WiredNetwork:
-
+		err = connectWired()
+		if err != nil {
+			return
+		}
 	case *network.WirelessNetwork:
 		lock.Lock()
 		defer lock.Unlock()

a6f1c50877353877d1b8996315838383ab627bdb
Zachary Huff <zach.huff.386@gmail.com>
2015-09-07 15:04:12 -0400

    Add commands to reset

--- wicd/reset.go
+++ wicd/reset.go
@@ -6,7 +6,32 @@ func Reset() (err error) {
 		return
 	}
 
-	_, err = conn.Call("SetSignalDisplayType", false)
+	_, err = conn.Call("SetForcedDisconnect", true)
+	if err != nil {
+		return
+	}
+
+	_, err = conn.Call("SetUseGlobalDNS", false)
+	if err != nil {
+		return
+	}
+
+	_, err = conn.Call("SetSignalDisplayType", uint32(0))
+	if err != nil {
+		return
+	}
+
+	_, err = conn.Call("SetAutoReconnect", false)
+	if err != nil {
+		return
+	}
+
+	_, err = conn.Call("SetWiredAutoConnectMethod", uint32(1))
+	if err != nil {
+		return
+	}
+
+	_, err = conn.Call("SetPreferWiredNetwork", false)
 	if err != nil {
 		return
 	}

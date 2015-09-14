33401850dc24e7ea4fe4e47c80b9bede8ea76b70
Zachary Huff <zach.huff.386@gmail.com>
2015-09-14 08:44:33 -0400

    Add hostapd

---
+++ hostapd/constants.go
@@ -0,0 +1,17 @@
+package hostapd
+
+const (
+	NetLink = "nl80211"
+	Realtek = "rtl871xdrv"
+)
+
+const conf = `driver=%s
+ssid=%s
+interface=%s
+channel=%d
+auth_algs=1
+wpa=2
+wpa_passphrase=%s
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP`

e0a1b3cc449e8beb36164d400e64be5b1786b57f
Zachary Huff <zach.huff.386@gmail.com>
2015-09-14 14:39:36 -0400

    Add dhcpd

---
+++ dhcpd/constants.go
@@ -0,0 +1,12 @@
+package dhcpd
+
+const conf = `subnet %s netmask %s {
+	range %s %s;
+	option subnet-mask %s;
+	option broadcast-address %s;
+	option routers %s;
+	option domain-name "%s";
+	option domain-name-servers %s;
+	default-lease-time 172800;
+	max-lease-time 172800;
+}`

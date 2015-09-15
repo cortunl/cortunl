58ca65e2bd3f1f256463b0b32ba52e69818e9cdd
Zachary Huff <zach.huff.386@gmail.com>
2015-09-14 20:17:05 -0400

    Add temp dir to constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,7 +1,12 @@
 package constants
 
+import (
+	"path/filepath"
+)
+
 const (
-	Name = "cortunl"
+	Name         = "cortunl"
 	NameFormated = "Cortunl"
-	Version = "0.1.0"
+	Version      = "0.1.0"
+	TempDir      = filepath.Join(filepath.Separator, "tmp")
 )

95417d0ccc7d9686ce1315f1a107fdea24b20b11
Zachary Huff <zach.huff.386@gmail.com>
2015-09-14 20:34:39 -0400

    Add get temp dir

--- utils/file.go
+++ utils/file.go
@@ -4,10 +4,23 @@ import (
 	"github.com/cortunl/cortunl/constants"
 	"github.com/dropbox/godropbox/errors"
 	"os"
+	"path/filepath"
 )
 
 func Create(path string) (file *os.File, err error) {
 	file, err = os.Create(path)
+	if err != nil {
+		err = &constants.WriteError{
+			errors.Wrapf(err, "utils: Failed to create '%s'", path),
+		}
+		return
+	}
+
+	return
+}
+
+func MkdirAll(path string) (err error) {
+	err = os.MkdirAll(path, 0755)
 	if err != nil {
 		err = &constants.WriteError{
 			errors.Wrapf(err, "utils: Failed to create '%s'", path),
@@ -30,6 +43,21 @@ func CreateWrite(path string, data string) (err error) {
 		err = &constants.WriteError{
 			errors.Wrapf(err, "utils: Failed to write to file '%s'", path),
 		}
+		return
+	}
+
+	return
+}
+
+func GetTempDir() (path string, err error) {
+	path = filepath.Join(
+		constants.TempDir,
+		string(filepath.Separator),
+		RandStr(16),
+	)
+
+	err = MkdirAll(path)
+	if err != nil {
 		return
 	}
 

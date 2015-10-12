29c04850b4610175bb7fcd3cbfc4563f6c01fec1
Zachary Huff <zach.huff.386@gmail.com>
2015-10-12 16:41:59 -0400

    Add ipv6 forwarding to utils

--- utils/misc.go
+++ utils/misc.go
@@ -9,7 +9,14 @@ func EnableIpForwarding() (err error) {
 	err = Exec("", "sysctl", "-w", "net.ipv4.ip_forward=1")
 	if err != nil {
 		err = &constants.ExecError{
-			errors.Wrap(err, "utils: Failed to enable IP forwarding"),
+			errors.Wrap(err, "utils: Failed to enable IPv4 forwarding"),
+		}
+		return
+	}
+	err = Exec("", "sysctl", "-w", "net.ipv6.conf.all.forwarding=1")
+	if err != nil {
+		err = &constants.ExecError{
+			errors.Wrap(err, "utils: Failed to enable IPv6 forwarding"),
 		}
 		return
 	}

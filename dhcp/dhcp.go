a63e0724e29b5be66fe3e3a065357c052b8b6e63
Zachary Huff <zach.huff.386@gmail.com>
2015-10-13 13:16:58 -0400

    Use dnsmasq for dhcp

--- dhcp/dhcp.go
+++ dhcp/dhcp.go
@@ -1,37 +1,100 @@
 package dhcp
 
 import (
+	"fmt"
+	"github.com/cortunl/cortunl/constants"
+	"github.com/cortunl/cortunl/settings"
+	"github.com/cortunl/cortunl/utils"
+	"github.com/dropbox/godropbox/errors"
 	"net"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 type Dhcp struct {
-	v4        *dhcp4
-	v6        *dhcp6
+	cmd       *exec.Cmd
+	path      string
 	Interface string
 	Network   *net.IPNet
 	Network6  *net.IPNet
 }
 
-func (d *Dhcp) Init() {
-	d.v4 = &dhcp4{
-		Interface: d.Interface,
-		Network:   d.Network,
+func (d *Dhcp) writeConf() (err error) {
+	d.path, err = utils.GetTempDir()
+	if err != nil {
+		return
+	}
+	d.path = filepath.Join(d.path, confName)
+
+	mask := net.IP(d.Network.Mask).String()
+	router := ""
+	start := ""
+	end := ""
+	for ip := range utils.IterNetwork(d.Network) {
+		if router == "" {
+			router = ip.String()
+		} else if start == "" {
+			start = ip.String()
+		} else {
+			end = ip.String()
+		}
 	}
 
-	d.v6 = &dhcp6{
-		Interface: d.Interface,
-		Network6:  d.Network6,
-	}
-}
+	router6 := utils.CopyIp(d.Network6.IP)
+	utils.IncIp(router6)
+	networkSplit6 := strings.Split(d.Network6.String(), "/")
+	network6 := networkSplit6[0]
+	cidr6 := networkSplit6[1]
 
-func (d *Dhcp) Start() (err error) {
-	err = d.v4.Start()
+	servers := ""
+	dnsServers := []string{}
+	dnsServers = append(dnsServers, settings.Settings.DnsServers...)
+	dnsServers = append(dnsServers, settings.Settings.DnsServers6...)
+	for _, svr := range dnsServers {
+		servers += fmt.Sprintf("server=%s\n", svr)
+	}
+
+	data := fmt.Sprintf(conf,
+		servers,
+		d.Interface,
+		start,
+		end,
+		mask,
+		network6,
+		cidr6,
+		router,
+		router,
+		settings.Settings.DnsServers[0],
+		router6,
+		settings.Settings.DnsServers6[0],
+	)
+
+	err = utils.CreateWrite(d.path, data)
 	if err != nil {
 		return
 	}
 
-	err = d.v6.Start()
+	return
+}
+
+func (d *Dhcp) Start() (err error) {
+	err = d.writeConf()
 	if err != nil {
+		return
+	}
+
+	d.cmd = exec.Command("dnsmasq", "--keep-in-foreground",
+		fmt.Sprintf("--conf-file=%s", d.path))
+	d.cmd.Stdout = os.Stdout
+	d.cmd.Stderr = os.Stdout
+
+	err = d.cmd.Start()
+	if err != nil {
+		err = &constants.ExecError{
+			errors.Wrap(err, "dhcp: Failed to exec dnsmasq"),
+		}
 		return
 	}
 
@@ -39,13 +102,15 @@ func (d *Dhcp) Start() (err error) {
 }
 
 func (d *Dhcp) Stop() (err error) {
-	err = d.v4.Stop()
-	if err != nil {
+	if d.cmd == nil {
 		return
 	}
 
-	err = d.v6.Stop()
+	err = d.cmd.Process.Kill()
 	if err != nil {
+		err = &constants.ExecError{
+			errors.Wrap(err, "dhcp: Failed to stop dnsmasq"),
+		}
 		return
 	}
 
@@ -53,13 +118,15 @@ func (d *Dhcp) Stop() (err error) {
 }
 
 func (d *Dhcp) Wait() (err error) {
-	err = d.v4.Wait()
-	if err != nil {
+	if d.cmd == nil {
 		return
 	}
 
-	err = d.v6.Wait()
+	err = d.cmd.Wait()
 	if err != nil {
+		err = &constants.ExecError{
+			errors.Wrap(err, "dhcp: Dnsmasq exec error"),
+		}
 		return
 	}
 

bebb1fe3f950e337a49ba1630a63531eb46830f5
Zachary Huff <zach.huff.386@gmail.com>
2015-10-13 13:16:18 -0400

    Update dhcp conf

--- dhcp/constants.go
+++ dhcp/constants.go
@@ -1,27 +1,16 @@
 package dhcp
 
 const (
-	confName  = "dhcpd.conf"
-	confName6 = "radvd.conf"
+	confName  = "dnsmasq.conf"
 )
 
-const conf = `subnet %s netmask %s {
-	range %s %s;
-	option subnet-mask %s;
-	option broadcast-address %s;
-	option routers %s;
-	option domain-name-servers %s;
-	default-lease-time 172800;
-	max-lease-time 172800;
-}`
-
-const conf6 = `interface %s {
-	AdvSendAdvert on;
-	MinRtrAdvInterval 3;
-	MaxRtrAdvInterval 10;
-	prefix %s {
-		AdvOnLink on;
-		AdvAutonomous on;
-		AdvRouterAddr on;
-	};
-};`
+const conf = `no-resolv
+domain-needed
+%sinterface=%s
+no-hosts
+dhcp-range=%s,%s,%s,12h
+dhcp-range=%s,ra-stateless,ra-names,%s
+dhcp-option=option:router,%s
+dhcp-option=option:dns-server,%s,%s
+dhcp-option=option6:dns-server,[%s],[%s]
+dhcp-authoritative`

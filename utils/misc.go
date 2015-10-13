aab16fd1c1b711165f279610b4323be7124a9120
Zachary Huff <zach.huff.386@gmail.com>
2015-10-13 13:19:23 -0400

    Improve ip forwarding

--- utils/misc.go
+++ utils/misc.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/cortunl/cortunl/constants"
 	"github.com/dropbox/godropbox/errors"
+	"net"
 )
 
 func EnableIpForwarding() (err error) {
@@ -12,6 +14,35 @@ func EnableIpForwarding() (err error) {
 			errors.Wrap(err, "utils: Failed to enable IPv4 forwarding"),
 		}
 		return
+	}
+
+	err = Exec("", "sysctl", "-w", "net.ipv6.conf.all.accept_ra=2")
+	if err != nil {
+		err = &constants.ExecError{
+			errors.Wrap(err, "utils: Failed to enable IPv6 accept_ra"),
+		}
+		return
+	}
+
+	err = Exec("", "sysctl", "-w", "net.ipv6.conf.default.accept_ra=2")
+	if err != nil {
+		err = &constants.ExecError{
+			errors.Wrap(err, "utils: Failed to enable IPv6 accept_ra"),
+		}
+		return
+	}
+
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		err = &constants.UnknownError{
+			errors.Wrap(err, "utils: Failed to get network interfaces"),
+		}
+		return
+	}
+
+	for _, iface := range ifaces {
+		_ = Exec("", "sysctl", "-w",
+			fmt.Sprintf("net.ipv6.conf.%s.accept_ra=2", iface.Name))
 	}
 
 	err = Exec("", "sysctl", "-w", "net.ipv6.conf.all.forwarding=1")
